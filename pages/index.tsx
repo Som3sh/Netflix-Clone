d3c93b2cc2f64fd74776b33453e30f306a5a4efc
Som3sh <someshmohanty08@gmail.com>
2023-12-26 02:59:56 +0530

    Update styles and main component in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
+    <main className="text-2xl text-red-500 font-bold">
       <h1>Netflix Clone</h1>
     </main>
   );
