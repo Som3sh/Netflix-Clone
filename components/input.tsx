e199b15b5d848b131495c82e36c13f173c0a867b
Som3sh <someshmohanty08@gmail.com>
2024-01-03 20:28:37 +0530

    Update dependencies and database configuration

--- components/input.tsx
+++ components/input.tsx
@@ -1,5 +1,5 @@
-import exp from "constants";
 import React from "react";
+
 interface InputProps {
   id: string;
   onChange: any;
@@ -13,8 +13,8 @@ const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
     <div className="relative">
       <input
         onChange={onChange}
-        type={type}
         value={value}
+        type={type}
         id={id}
         className="
         block
@@ -24,39 +24,40 @@ const Input: React.FC<InputProps> = ({ id, onChange, value, label, type }) => {
         pb-1
         w-full
         text-md
-        text-white
-        bg-neutral-900
+      text-white
+      bg-neutral-700
         appearance-none
         focus:outline-none
         focus:ring-0
         peer
-    
-    
-      "
-        placeholder="  "
+        invalid:border-b-1
+        "
+        placeholder=" "
       />
       <label
-        className="absolute 
-        text-md 
-        text-zinc-400
+        htmlFor={id}
+        className="
+        absolute 
+        text-md
+      text-zinc-400
         duration-150 
-        transform
-        -translate-y-3
+        transform 
+        -translate-y-3 
         scale-75 
         top-4 
         z-10 
         origin-[0] 
-        left-6 
+        left-6
         peer-placeholder-shown:scale-100 
         peer-placeholder-shown:translate-y-0 
         peer-focus:scale-75
         peer-focus:-translate-y-3
-        "
-        htmlFor={id}
+      "
       >
         {label}
       </label>
     </div>
   );
 };
+
 export default Input;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,37 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-
+import { getSession, signOut } from "next-auth/react";
+import { NextPage, NextPageContext } from "next";
+import useCurrentUser from "@/hooks/useCurrentuser";
 const inter = Inter({ subsets: ["latin"] });
 
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
+  const { data: user } = useCurrentUser();
   return (
     <main className="text-2xl text-red-500 font-bold">
       <h1>Netflix Clone</h1>
+      <p>Logged in as {user?.name}</p>
+      <button
+        className="h-10 w-full bg-white "
+        onClick={() => signOut()}
+      ></button>
     </main>
   );
 }
